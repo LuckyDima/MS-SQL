87a31745779c8fe693fbc00d49620fc3ce5e21db
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2022-03-20 15:29:36 +0300

    Update Recursion date cte.sql

--- Tricks/Recursion date cte.sql
+++ Tricks/Recursion date cte.sql
@@ -8,3 +8,20 @@ WITH Days(D) AS
 )
 SELECT D FROM Days ORDER BY D
 OPTION (MAXRECURSION 0);
+
+
+
+;with cte as
+(
+select 1 n
+union all
+select n + 1 from cte where n < 20
+)
+,cte2 as
+(
+select n * cast(rand() as decimal(2,1)) rnd from cte
+)
+select 
+count(rnd) cnt, min(rnd) min, max(rnd) max
+from cte2 
+group by (iif(cte2.rnd<=2,0,iif(cte2.rnd >2 and cte2.rnd <=5,1,2)))
