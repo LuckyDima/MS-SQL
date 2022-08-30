6964ca82f628b0e8df72b2ba57e1e0792ac0277f
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2022-08-30 17:54:52 +0300

    Update What necessary to sql optimizator.sql

--- Troubleshooting/What necessary to sql optimizator.sql
+++ Troubleshooting/What necessary to sql optimizator.sql
@@ -1,29 +1,18 @@
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
 
 Declare @StringToSearchFor Varchar(255)
-
 Set @StringToSearchFor = '%<MissingIndexes>%'
 
 SELECT  st.text
-
       ,cp.cacheobjtype
-
       ,cp.objtype
-
       ,DB_NAME(st.dbid) AS [DatabaseName]
-
       ,cp.usecounts
-
       ,qp.query_plan
-
 FROM sys.dm_exec_cached_plans cp
-
       CROSS APPLY sys.dm_exec_sql_text(cp.plan_handle) st
-
       CROSS APPLY sys.dm_exec_query_plan(cp.plan_handle) qp
-
 WHERE CAST(qp.query_plan AS NVARCHAR(MAX))LIKE @StringToSearchFor
-
 ORDER BY cp.usecounts DESC
 
 Примечание: можно изменить значение @Stringtosearchfor на :
