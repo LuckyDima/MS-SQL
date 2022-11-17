a748b1556497bc89980bc21cc40cad1cc05720e8
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2022-11-17 23:10:39 +0400

    Update Low memory test.sql

--- Troubleshooting/Memory/Low memory test.sql
+++ Troubleshooting/Memory/Low memory test.sql
@@ -1,29 +1,32 @@
--- Быстрый тест проблем с памятью
--- По мотивам: http://bit.ly/LkT05M
-WITH RingBufferXML
+
+
+
+-- Quick test for memory
+-- http://bit.ly/LkT05M
+;WITH RingBufferXML
 AS(SELECT CAST(Record AS XML) AS RBR FROM sys .dm_os_ring_buffers
    WHERE ring_buffer_type = 'RING_BUFFER_RESOURCE_MONITOR'
   )
-SELECT DISTINCT 'Зафиксированы проблемы' =
+SELECT DISTINCT 'Issues' =
           CASE
                     WHEN XMLRecord.value('(ResourceMonitor/IndicatorsProcess)[1]','tinyint')  = 0 AND
                          XMLRecord.value('(ResourceMonitor/IndicatorsSystem)[1]','tinyint')   = 2 
-                    THEN 'Недостаточно физической памяти для системы'
+                    THEN 'Not enoght memory for system'
                     WHEN XMLRecord.value('(ResourceMonitor/IndicatorsProcess)[1]','tinyint')  = 0 AND 
                          XMLRecord.value('(ResourceMonitor/IndicatorsSystem)[1]','tinyint')   = 4 
-                    THEN 'Недостаточно виртуальной памяти для системы' 
+                    THEN 'Not enoght virtual memory for system' 
                     WHEN XMLRecord.value('(ResourceMonitor/IndicatorsProcess)[1]', 'tinyint') = 2 AND 
                          XMLRecord.value('(ResourceMonitor/IndicatorsSystem)[1]','tinyint')   = 0 
-                    THEN'Недостаточно физической памяти для запросов'
+                    THEN'Not enoght memory for queries'
                     WHEN XMLRecord.value('(ResourceMonitor/IndicatorsProcess)[1]', 'tinyint') = 4 AND 
                          XMLRecord.value('(ResourceMonitor/IndicatorsSystem)[1]', 'tinyint')  = 4
-                    THEN 'Недостаточно виртуальной памяти для запросов и системы'
+                    THEN 'Not enoght virtual memory for system and queries'
                     WHEN XMLRecord.value('(ResourceMonitor/IndicatorsProcess)[1]','tinyint')  = 2 AND 
                          XMLRecord.value('(ResourceMonitor/IndicatorsSystem)[1]','tinyint')   = 4 
-                    THEN 'Недостаточно виртуальной памяти для системы и физической для запросов'
+                    THEN 'Not enoght virtual memory for system and usual memmory for queries'
                     WHEN XMLRecord.value('(ResourceMonitor/IndicatorsProcess)[1]', 'tinyint') = 2 AND 
                          XMLRecord.value('(ResourceMonitor/IndicatorsSystem)[1]', 'tinyint')  = 2 
-                    THEN 'Недостаточно физической памяти для системы и запросов'
+                    THEN 'Not enoght physical memory for system and queries'
          END
 FROM        RingBufferXML
 CROSS APPLY RingBufferXML.RBR.nodes ('Record') Record (XMLRecord)
