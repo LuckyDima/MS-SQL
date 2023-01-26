4b80291d938af8f2af521770ad6dbdd352c14949
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2023-01-26 14:07:32 +0400

    Update Indexes need to add 2.sql

--- Indexes/Indexes need to add 2.sql
+++ Indexes/Indexes need to add 2.sql
@@ -1,16 +1,16 @@
 -- Не забудьте поправить сценарий создания индексов, 
 -- убрав лишние запятые или пустую инструкцию INCLUDE
-SELECT 	[Ðåêîìåíäóåìûé èíäåêñ]= 
+SELECT 	[Рекомендуемый индекс]= 
 		 '-- CREATE INDEX [IX_' + OBJECT_NAME(mid.object_id) + '_' +
 		CAST(mid.index_handle AS nvarchar) + '] ON ' + 
 		mid.statement + ' (' + ISNULL(mid.equality_columns,'') + 
 		', ' + ISNULL(mid.inequality_columns,'') + 
 		') INCLUDE (' + ISNULL(mid.included_columns,'') + ');', 
-	[×èñëî êîìïèëÿöèé] = migs.unique_compiles,
-	[Êîëè÷åñòâî îïåðàöèé ïîèñêà] = migs.user_seeks,
-	[Êîëè÷åñòâî îïåðàöèé ïðîñìîòðà] = migs.user_scans,
-	[Ñðåäíÿÿ ñòîèìîñòü ] = CAST(migs.avg_total_user_cost AS int),
-	[Ñðåäíèé ïðîöåíò âûèãðûøà] = CAST(migs.avg_user_impact AS int)
+	[Число компиляций] = migs.unique_compiles,
+	[Количество операций поиска] = migs.user_seeks,
+	[Количество операций просмотра] = migs.user_scans,
+	[Средняя стоимость ] = CAST(migs.avg_total_user_cost AS int),
+	[Средний процент выигрыша] = CAST(migs.avg_user_impact AS int)
 FROM	sys.dm_db_missing_index_groups mig
 JOIN	sys.dm_db_missing_index_group_stats migs 
 ON	migs.group_handle = mig.index_group_handle
