75ecb60e559805ba9a491b693f6a36a6cd296702
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2024-03-25 20:00:06 +0300

    Update CheckDatabaseSettings.sql

--- Policy/Policies/CheckDatabaseSettings.sql
+++ Policy/Policies/CheckDatabaseSettings.sql
@@ -1,20 +1,40 @@
-Declare @object_set_id int
-EXEC msdb.dbo.sp_syspolicy_add_object_set @object_set_name=N'Check database settings_ObjectSet', @facet=N'Database', @object_set_id=@object_set_id OUTPUT
-Select @object_set_id
+DECLARE @object_set_id INT;
+EXEC msdb.dbo.sp_syspolicy_add_object_set @object_set_name = N'Check database settings_ObjectSet',
+                                          @facet = N'Database',
+                                          @object_set_id = @object_set_id OUTPUT;
+SELECT @object_set_id;
 
-Declare @target_set_id int
-EXEC msdb.dbo.sp_syspolicy_add_target_set @object_set_name=N'Check database settings_ObjectSet', @type_skeleton=N'Server/Database', @type=N'DATABASE', @enabled=True, @target_set_id=@target_set_id OUTPUT
-Select @target_set_id
+DECLARE @target_set_id INT;
+EXEC msdb.dbo.sp_syspolicy_add_target_set @object_set_name = N'Check database settings_ObjectSet',
+                                          @type_skeleton = N'Server/Database',
+                                          @type = N'DATABASE',
+                                          @enabled = True,
+                                          @target_set_id = @target_set_id OUTPUT;
+SELECT @target_set_id;
 
-EXEC msdb.dbo.sp_syspolicy_add_target_set_level @target_set_id=@target_set_id, @type_skeleton=N'Server/Database', @level_name=N'Database', @condition_name=N'Only user databases', @target_set_level_id=0
+EXEC msdb.dbo.sp_syspolicy_add_target_set_level @target_set_id = @target_set_id,
+                                                @type_skeleton = N'Server/Database',
+                                                @level_name = N'Database',
+                                                @condition_name = N'Only user databases',
+                                                @target_set_level_id = 0;
 
 
 GO
 
-Declare @policy_id int
-EXEC msdb.dbo.sp_syspolicy_add_policy @name=N'Check database settings', @condition_name=N'Check database settings NOT PROD', @policy_category=N'Internal Check', @description=N'', @help_text=N'', @help_link=N'', @schedule_uid=N'', @execution_mode=4, @is_enabled=True, @policy_id=@policy_id OUTPUT, @root_condition_name=N'', @object_set=N'Check database settings_ObjectSet'
-Select @policy_id
+DECLARE @policy_id INT;
+EXEC msdb.dbo.sp_syspolicy_add_policy @name = N'Check database settings',
+                                      @condition_name = N'Check database settings NOT PROD',
+                                      @policy_category = N'Internal Check',
+                                      @description = N'',
+                                      @help_text = N'',
+                                      @help_link = N'',
+                                      @schedule_uid = N'',
+                                      @execution_mode = 4,
+                                      @is_enabled = True,
+                                      @policy_id = @policy_id OUTPUT,
+                                      @root_condition_name = N'',
+                                      @object_set = N'Check database settings_ObjectSet';
+SELECT @policy_id;
 
 
 GO
-
