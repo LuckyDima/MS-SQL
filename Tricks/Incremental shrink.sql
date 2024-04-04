0052cdb8f1b849d891eec768d4d47058d13437df
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2024-04-04 18:39:46 +0300

    Update Incremental shrink.sql

--- Tricks/Incremental shrink.sql
+++ Tricks/Incremental shrink.sql
@@ -9,21 +9,21 @@ DECLARE @ShrinkStepMB BIGINT = 100;
 IF @DbName IS NULL RETURN;
 
 DECLARE file_cursor CURSOR FAST_FORWARD LOCAL FORWARD_ONLY FOR 
-    SELECT name FROM sys.master_files WHERE type_desc = 'ROWS';
+    SELECT name FROM sys.master_files WHERE type_desc = 'ROWS' AND database_id = DB_ID(@DbName);
 
 OPEN file_cursor;
 FETCH NEXT FROM file_cursor INTO @LogicalFileName;
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-    SELECT @FileSizeMB = size/128., @UsedMB = FILEPROPERTY(@LogicalFileName, 'SpaceUsed')/128. FROM sys.sysfiles WHERE name = @LogicalFileName;
+    SELECT @FileSizeMB = size/128., @UsedMB = FILEPROPERTY(@LogicalFileName, 'SpaceUsed')/128. FROM sys.master_files WHERE name = @LogicalFileName AND database_id = DB_ID(@DbName);
     WHILE @FileSizeMB > @UsedMB + @FreeSizeMB + @ShrinkStepMB
     BEGIN
         SET @Sql = 'USE ' + QUOTENAME(@DbName) + '; DBCC SHRINKFILE (' + QUOTENAME(@LogicalFileName) + ', ' + CONVERT(VARCHAR(20), @FileSizeMB - @ShrinkStepMB) + ') WITH NO_INFOMSGS';
         RAISERROR (N'Start %s', 0, 1, @Sql) WITH NOWAIT;
         EXEC (@Sql);
         RAISERROR('Done %s', 0, 1, @Sql) WITH NOWAIT;
-        SELECT @FileSizeMB = size/128., @UsedMB = FILEPROPERTY(@LogicalFileName, 'SpaceUsed')/128. FROM sys.sysfiles WHERE name = @LogicalFileName;
+        SELECT @FileSizeMB = size/128., @UsedMB = FILEPROPERTY(@LogicalFileName, 'SpaceUsed')/128. FROM sys.master_files WHERE name = @LogicalFileName AND database_id = DB_ID(@DbName);
     END;
     FETCH NEXT FROM file_cursor INTO @LogicalFileName;
 END;
