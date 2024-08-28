a964f5035936dd965fd026d106385d0b8b442d05
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2024-08-28 12:17:48 +0300

    Add files via upload

---
+++ WindowsTunning/ServicesDisabled.ps1
@@ -0,0 +1,30 @@
+﻿# Список служб для перевода в состояние Disabled
+$servicesToDisable = @(
+    "AppReadiness",
+    "BalloonService",
+    "CDPSvc",
+    "CDPUserSvc_50dc38",
+    "DPS",
+    "FontCache",
+    "iphlpsvc",
+    "lmhosts",
+    "NcbService",
+    "PcaSvc",
+    "QEMU-GA",
+    "SysMain",
+    "TabletInputService",
+    "TokenBroker",
+    "TrkWks",
+    "UALSVC",
+    "WdNisSvc",
+    "WinDefend",
+    "wmiApSrv",
+    "WpnService",
+    "WpnUserService_50dc38"
+)
+
+# Перевод каждой службы в состояние Disabled
+foreach ($service in $servicesToDisable) {
+    Set-Service -Name $service -StartupType Disabled
+    Write-Host "Service $service has been set to Disabled."
+}
---
+++ WindowsTunning/SetBackgroundPicture.ps1
@@ -0,0 +1,21 @@
+﻿$serverName = $env:COMPUTERNAME
+$os = (Get-WmiObject Win32_OperatingSystem).Caption
+$cpu = (Get-WmiObject Win32_Processor).Name
+$ram = [math]::round((Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory / 1GB, 2)
+
+$text = "Server:  $serverName`nOS:      $os`nCPU:    $cpu`nRAM:    $ram GB"
+
+$image = New-Object Drawing.Bitmap 1920,1080
+$graphics = [Drawing.Graphics]::FromImage($image)
+$graphics.Clear([Drawing.Color]::Black)
+$font = New-Object Drawing.Font "Arial", 10
+$brush = [Drawing.Brushes]::White
+$graphics.DrawString($text, $font, $brush, [Drawing.PointF]::new(50,50))
+
+$outputPath = "C:\Windows\BackgroundPicture.jpg"
+$image.Save($outputPath, [Drawing.Imaging.ImageFormat]::Jpeg)
+
+$user = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$regPath = "HKCU:\Control Panel\Desktop"
+Set-ItemProperty -Path $regPath -Name Wallpaper -Value $outputPath
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
