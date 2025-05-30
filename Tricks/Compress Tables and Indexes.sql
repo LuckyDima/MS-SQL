a0d4492ff2f3a3071a11c9fc012381d1038519ec
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2025-05-30 11:54:53 +0300

    Update Compress Tables and Indexes.sql

--- Tricks/Compress Tables and Indexes.sql
+++ Tricks/Compress Tables and Indexes.sql
@@ -25,3 +25,14 @@ WHERE o.type = 'U'
   AND ps.[reserved_page_count] > 10000 
   AND p.data_compression_desc != 'PAGE'
 ORDER BY ps.[reserved_page_count];
+
+--Set advanced settings to reduce competition on the last page of the index
+SET NOCOUNT ON
+SELECT 'ALTER INDEX [' + i.[name] + '] ON [' + s.[name] + '].[' + o.[name] + '] SET (OPTIMIZE_FOR_SEQUENTIAL_KEY = ON);'
+FROM sys.objects AS o WITH (NOLOCK)
+JOIN sys.indexes AS i WITH (NOLOCK) ON o.[object_id] = i.[object_id]
+JOIN sys.schemas AS s WITH (NOLOCK) ON o.[schema_id] = s.[schema_id]
+WHERE o.[type] = 'U' 
+  AND i.[index_id] > 0 
+  AND i.optimize_for_sequential_key = 0
+ORDER BY o.[name], i.[name];
